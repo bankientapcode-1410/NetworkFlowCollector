b95a984cd2d77b51013e5dbc8c1323e52177b9e8
bankientapcode-1410 <nhkiencuong@gmail.com>
2026-06-27 19:40:30 +0700

    fix

--- src/main/java/com/kien/networkflowcollector/NetworkFlowCollectorApplication.java
+++ src/main/java/com/kien/networkflowcollector/NetworkFlowCollectorApplication.java
@@ -3,10 +3,11 @@ package com.kien.networkflowcollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.kien.networkflowcollector")
 public class NetworkFlowCollectorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NetworkFlowCollectorApplication.class, args);
     }
+
 }
